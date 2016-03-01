094f98fd5151426cac2324b26dd0cbb4e0831b0e
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-03-01 22:03:33 +0100

    Analytics gehitu

---
+++ js/analytics.js
@@ -0,0 +1,23 @@
+var _gaq = _gaq || [];
+
+_gaq.push(['_setAccount', 'UA-33197095-1']);
+
+_gaq.push(['_setDomainName', '.argia.eus']);
+
+_gaq.push(['_trackPageview']);
+
+_gaq.push(['tracker2._setAccount', 'UA-4220678-1']);
+
+_gaq.push(['tracker2._setDomainName', '.argia.eus']);
+
+_gaq.push(['tracker2._trackPageview']);
+
+(function() {
+
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/u/ga.js';
+
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+})();
